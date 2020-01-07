ef899efb1d07257f85829c00b1163e4d44195389
maeva <maeva.moisan@epitech.eu>
2020-01-07 16:22:41 +0100

    Added form connexion

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Login from './Components/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Salut !</h1>
+      <Login/>
     </div>
   );
 }
