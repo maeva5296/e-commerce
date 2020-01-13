d5ed317ebe5e36a978dbb30ab478c319203b2dbf
maeva <maeva.moisan@epitech.eu>
2020-01-13 16:00:21 +0100

    ariane WIP

--- client/src/Components/Ariane/Ariane.js
+++ client/src/Components/Ariane/Ariane.js
@@ -1,52 +1,15 @@
 import React from "react";
 import './Ariane.css';
 
-
 class Ariane extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        error: null,
-        isLoaded: false,
-        categorie: []
-      };
-    }
-
-    componentDidMount() {
-        fetch("https://127.0.0.1:8000/api/categories")
-          .then(res => res.json())
-          .then(
-            (result) => {
-              this.setState({
-                isLoaded: true,
-                categorie: result['hydra:member']
-              });
-            },
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-              });
-            }
-          )
-      }
-
       render() {
-        const { error, isLoaded, categorie } = this.state;
-        if (error) {
-          return <div>Erreur : {error.message}</div>;
-        } else if (!isLoaded) {
-          return <div>Chargement…</div>;
-        } else {
             return (
             <div id="form" className="container">
                 <div className="row justify-content">
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="#"><i className="fas fa-home"></i></a></li>
-                            {categorie.map(categorie => (
-                                <li class="breadcrumb-item active" aria-current="page">{ categorie.name }</li>
-                            ))}
+                            <li className="breadcrumb-item"><a href="#"><i className="fas fa-home"></i></a></li>
+                            <li className="breadcrumb-item active" aria-current="page">{this.props.nameCategorie}</li>
                         </ol>
                     </nav>
                 </div>
@@ -54,6 +17,5 @@ class Ariane extends React.Component {
             );
         }
     }
-}
 
 export default Ariane;
