8d63342b281c4eca1984974c53ac4dcd7a855b1a
Aurélien <aurelien.ruiz-minano@epitech.eu>
2020-01-14 11:33:42 +0100

    Component BestArtcile OK

--- client/src/Components/BestArticles/BestArticles.js
+++ client/src/Components/BestArticles/BestArticles.js
@@ -16,7 +16,6 @@ class BestArticles extends Component {
         .then(res => res.json())
         .then(
             (result) => {
-                console.log(result['hydra:member'])
             this.setState({
                 isLoaded: true,
                 BestArticle: result['hydra:member']
@@ -38,17 +37,24 @@ class BestArticles extends Component {
         } else if (!isLoaded) {
         return <div>Chargement…</div>;
         } else {
-            return (
-                <div className="col">
-                    {BestArticle.map(BestArticle => (
-                        <div className="card" id="best-articles-card">
-                            <img src={ Config.url + "/images/products/msi-rtx.jpg" } className="card-img-top img-thumbnail" alt="images product" />
-                            <div className="card-body text-center">
-                                <h5 className="card-title">MSI RTX 2060 super Gaming X</h5>
-                                <a href="#" className="btn btn-warning pr-5 pl-5 font-weight-bold">Voir</a>
-                            </div>
+            let allBestArticles = [];
+            for(var i=0; i < 5; i++) {
+                allBestArticles.push(
+                    <div className="col">
+                    <div className="card" id="best-articles-card">
+                        <img src={ Config.url + "/" + BestArticle[i]['image'] } className="card-img-top img-thumbnail" alt="images product" />
+                        <div className="card-body text-center">
+                            <h5 className="card-title">{ BestArticle[i]['name'] }</h5>
+                            <p className="text-center text-success">{ BestArticle[i]['visits'] } Vus</p>
+                            <a href="#" className="btn btn-warning pr-5 pl-5 font-weight-bold">Voir</a>
                         </div>
-                    ))}
+                    </div>
+                </div>
+                )
+            }
+            return (
+                <div className="flex">
+                    { allBestArticles }
                 </div>
             );
         }
