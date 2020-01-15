5dd5a2e5cd979136c1d92cdcdbde35f0331aaeae
maeva <maeva.moisan@epitech.eu>
2020-01-15 18:08:09 +0100

    fil ariane ok

--- client/src/Components/Ariane/Ariane.js
+++ client/src/Components/Ariane/Ariane.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Ariane.css';
 
 class Ariane extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+      }
+    
+      render() {
         return (
         <div id="form" className="container">
             <div className="row justify-content">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
-                        <li className="breadcrumb-item"><a href="#"><i className="fas fa-home"></i></a></li>
-                        <li className="breadcrumb-item active" aria-current="page">{this.props.nameCategorie}</li>
+                        <li className="breadcrumb-item"><a href="/"><i className="fas fa-home"></i></a></li>
+                        <Link to={`/articles/${this.props.idCategorie}`}>
+                        <li className="breadcrumb-item"><a>{this.props.nameCategorie}</a></li>
+                        </Link>
+                        <li className="breadcrumb-item active" aria-current="page">{this.props.nameArticle}</li>
                     </ol>
                 </nav>
             </div>
