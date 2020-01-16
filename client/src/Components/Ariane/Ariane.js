efd5e831c0484aaaa2e162bc833454f62aa2031f
Aurélien <aurelien.ruiz-minano@epitech.eu>
2020-01-16 11:48:00 +0100

    / Ariane

--- client/src/Components/Ariane/Ariane.js
+++ client/src/Components/Ariane/Ariane.js
@@ -13,9 +13,9 @@ class Ariane extends React.Component {
             <div className="row justify-content">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
-                        <li className="breadcrumb-item"><a href="/"><i className="fas fa-home"></i></a></li>
+                        <li className="breadcrumb-item"><a href="/"><i className="fas fa-home"></i> / </a></li>
                         <Link to={`/articles/${this.props.idCategorie}`}>
-                        <li className="breadcrumb-item"><a>{this.props.nameCategorie}</a></li>
+                        <li className="breadcrumb-item"><a>{this.props.nameCategorie}</a> / </li>
                         </Link>
                         <li className="breadcrumb-item active" aria-current="page">{this.props.nameArticle}</li>
                     </ol>
